e9b2ba3acdd457bbe3e3a22fe250c1da82cff846
Jawann <jawann@gmail.com>
2018-02-13 04:31:14 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,9 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT gpa FROM students ORDER BY gpa LIMIT 1"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
